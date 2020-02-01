18f8765d2bb11bbd029099e53f6c01e299bde021
Gustavo de Miranda <gustavomirandag@gmail.com>
2020-02-01 15:54:57 -0300

    Add project files.

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Threading;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var myThread = new Thread(DoSomething);
+            myThread.Start();
+            Console.WriteLine("Hello World!");
+        }
+
+        private static void DoSomething()
+        {
+            Thread.Sleep(5000);
+        }
+    }
+}
---
+++ WebApp/Contexts/ProductContext.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApp.Models;
+
+namespace WebApp.Contexts
+{
+    public class ProductContext : DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+
+        public ProductContext()
+        {
+            Database.EnsureCreated();
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+
+            optionsBuilder.UseSqlServer("Server=tcp:task-webapp-db-server.database.windows.net,1433;Initial Catalog=task-webapp-db;Persist Security Info=False;User ID=pivotto;Password=@dsInf123;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
+        }
+    }
+}
